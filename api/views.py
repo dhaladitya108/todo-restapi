7c42521de9a5d83d1a95aa4cd846d933a2601a27
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2020-08-01 01:09:05 +0530

    👷 added all delete, create, update methods

--- api/views.py
+++ api/views.py
@@ -23,3 +23,35 @@ def taskList(request):
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id=pk)
+    # serializer = TaskSerializer(instance=task, data=request.data)
+    serializer = TaskSerializer(task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+    return Response(f'{task.title} delete')
